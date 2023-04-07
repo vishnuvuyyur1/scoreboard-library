d391cb9695c4cafb5ab6706c7ba4fc88cce7976c
vishnu <vishnu.vuyyur@gmail.com>
2023-04-07 12:06:45 +0200

    created a model package with model classes

---
+++ src/main/java/com/sportradar/scoreboard/model/ScoreBoard.java
@@ -0,0 +1,9 @@
+package com.sportradar.scoreboard.model;
+
+public class ScoreBoard {
+	Team homeTeam;
+	int homeTeamScore;
+	Team awayTeam;
+	int awayTeamScore;
+	long startTime;
+}
---
+++ src/main/java/com/sportradar/scoreboard/model/Team.java
@@ -0,0 +1,21 @@
+package com.sportradar.scoreboard.model;
+
+public enum Team {
+    Mexico,
+    Canada,
+    Spain,
+    Brazil,
+    Germany,
+    France,
+    Uruguay,
+    Italy,
+    Argentina,
+    Australia,
+    Monaco,
+    Andorra,
+    Iceland,
+    Malta,
+    Luxembourg,
+    Estonia,
+	Latvia
+}
