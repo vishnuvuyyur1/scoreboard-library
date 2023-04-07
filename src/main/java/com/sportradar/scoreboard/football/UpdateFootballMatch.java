66ad44cfbce4ead48fc7c0d17c78e4ba60f66543
vishnu <vishnu.vuyyur@gmail.com>
2023-04-07 13:21:40 +0200

    added interface for our  operation update score

---
+++ src/main/java/com/sportradar/scoreboard/football/UpdateFootballMatch.java
@@ -0,0 +1,16 @@
+package com.sportradar.scoreboard.football;
+
+import java.util.Map;
+
+import com.sportradar.scoreboard.model.ScoreBoard;
+import com.sportradar.scoreboard.operations.IUpdateMatch;
+
+public class UpdateFootballMatch implements IUpdateMatch {
+
+	@Override
+	public void updateScores(Map<String, ScoreBoard> scoreBoard, String matchId, int homeTeamScore, int awayTeamScore) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
