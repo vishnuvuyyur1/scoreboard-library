06f9431efc13e2c555a565908a61cefb8880bd48
vishnu <vishnu.vuyyur@gmail.com>
2023-04-07 14:35:10 +0200

    code format

--- src/main/java/com/sportradar/scoreboard/orchestrator/FootballOrchestrator.java
+++ src/main/java/com/sportradar/scoreboard/orchestrator/FootballOrchestrator.java
@@ -13,30 +13,31 @@ import com.sportradar.scoreboard.model.Team;
 public class FootballOrchestrator {
 	private Map<String, ScoreBoard> footballScoreBoard;
 	private final GetFootBallMatch getFootBall;
-    private final StartStopFootballMatch startStopFootball;
-    private final UpdateFootballMatch updateFootball;
-	
-    public FootballOrchestrator(GetFootBallMatch getFootBall, StartStopFootballMatch startStopFootball,
+	private final StartStopFootballMatch startStopFootball;
+	private final UpdateFootballMatch updateFootball;
+
+	public FootballOrchestrator(GetFootBallMatch getFootBall, StartStopFootballMatch startStopFootball,
 			UpdateFootballMatch updateFootball) {
 		this.getFootBall = getFootBall;
 		this.startStopFootball = startStopFootball;
 		this.updateFootball = updateFootball;
-		this.footballScoreBoard = new HashMap<>();;
+		this.footballScoreBoard = new HashMap<>();
+		;
 	}
-    
-    public String startNewGame(Team homeTeam, Team awayTeam) {
-    	return startStopFootball.startGame(this.footballScoreBoard, homeTeam, awayTeam);
-    }
-    
-    public void stopGame(String matchId) {
-        startStopFootball.finishGame(this.footballScoreBoard, matchId);
-    }
-    
-    public void updateScores(String matchId, int homeTeamScore, int awayTeamScore) {
-    	updateFootball.updateScores(this.footballScoreBoard, matchId, homeTeamScore, awayTeamScore);
-    } 
-    
-    public List<ScoreBoard> getGames() {
-    	return getFootBall.getGamesOrderedByTotalScore(this.footballScoreBoard);
-    }
+
+	public String startNewGame(Team homeTeam, Team awayTeam) {
+		return startStopFootball.startGame(this.footballScoreBoard, homeTeam, awayTeam);
+	}
+
+	public void stopGame(String matchId) {
+		startStopFootball.finishGame(this.footballScoreBoard, matchId);
+	}
+
+	public void updateScores(String matchId, int homeTeamScore, int awayTeamScore) {
+		updateFootball.updateScores(this.footballScoreBoard, matchId, homeTeamScore, awayTeamScore);
+	}
+
+	public List<ScoreBoard> getGames() {
+		return getFootBall.getGamesOrderedByTotalScore(this.footballScoreBoard);
+	}
 }
